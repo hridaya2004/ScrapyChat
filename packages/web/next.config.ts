91b6e4ddf1277f8344f0d4aad1b03e7da89f00a4
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-10-07 16:38:49 +0545

    chore(web): Run `bun run format`

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -4,8 +4,8 @@ import path from "node:path";
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
-    root: path.join(__dirname, "../..")
-  }
+    root: path.join(__dirname, "../.."),
+  },
 };
 
 export default nextConfig;
