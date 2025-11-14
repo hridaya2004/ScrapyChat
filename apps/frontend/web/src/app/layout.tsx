c84562457cdda6bc31bf128f20083291ea77295b
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-11-14 09:43:18 +0545

    chore(web): Move font config to libs/

--- apps/frontend/web/src/app/layout.tsx
+++ apps/frontend/web/src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { siteConfig } from "@/config/global";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from "@/lib/geist";
 
 export const metadata: Metadata = {
   title: siteConfig.title,
