a71e8e59a1aa49a686295ddc0c3d521b92249690
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-11-23 10:54:20 +0545

    chore: Properly decode JWT token
    * Hardcode the JWK for now for local development

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,8 +1,56 @@
-import json
 from base64 import urlsafe_b64decode
 
 from fastapi import Request
 from fastapi.exceptions import HTTPException
+from jose import jwt
+
+jwks = {
+    "keys": [
+        {
+            "crv": "Ed25519",
+            "kty": "OKP",
+            "x": "ylWh8tootR_atiNkT84y-yMsQ-9Vh3tLuQhEKLEj5sE",
+            "kid": "606cda0d-cef6-40a7-b1ac-b2cd8c21646e",
+            "use": "sig",
+            "alg": "EdDSA",
+        }
+    ]
+}
+
+# TODO: Grab JWKS from here
+jwks_url = "https://scrapychat.hridaya.tech/.well-known/jwks.json"
+
+
+def get_public_key(jwks, kid):
+    for key in jwks["keys"]:
+        # Decode public key using kid
+        if key["kid"] == kid:
+            x = key["x"]
+            return urlsafe_b64decode(x + "=" * (4 - len(x) % 4))
+    raise HTTPException(status_code=401, detail="Unknown key ID")
+
+
+def verify_token(token: str):
+    try:
+        header = jwt.get_unverified_header(token)
+        kid = header.get("kid")
+        if not kid:
+            raise HTTPException(status_code=401, detail="Missing kid")
+
+        public_key = get_public_key(jwks, kid)
+
+        decoded = jwt.decode(
+            token,
+            public_key,
+            algorithms=["EdDSA"],
+        )
+
+        return decoded
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Signature expired")
+    except jwt.JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 
 def get_user(request: Request):
@@ -17,12 +65,6 @@ def get_user(request: Request):
     if len(parts) != 2 or parts[0].lower() != "bearer":
         raise HTTPException(status_code=401, detail="Unauthorized")
 
-    access_token = parts[1]
-
-    # TODO: decode the token properly
-    encoded_payload = access_token.split(".")[1]
-    padding = "=" * (4 - len(encoded_payload) % 4)
-    decoded_payload = urlsafe_b64decode(encoded_payload + padding).decode()
-    json_payload = json.loads(decoded_payload)
-
-    return json_payload.get("id")
+    token = parts[1]
+    decoded = verify_token(token)
+    return decoded["id"]
