8f8571d119e5559ccc5dc6e5c2d60f13b1c075c6
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-12-22 15:36:17 +0545

    chore(web): Hardcode URL for FastAPI apiConfig

--- apps/frontend/web/src/config/global.ts
+++ apps/frontend/web/src/config/global.ts
@@ -4,6 +4,9 @@ export const siteConfig = {
 } as const;
 
 export const apiConfig = {
+  // TODO: Fix prod URL after FastAPI changes are finished
   baseUrl:
-    process.env.NODE_ENV === "development" ? "http://localhost:8080" : "/api",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080"
+      : "http://localhost:8080/api",
 } as const;
