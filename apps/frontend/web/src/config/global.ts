0d73a600b28f6b0e417ae840cb75e1d031654665
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-12-30 21:12:32 +0545

    chore(web): Update apiConfig for FastAPI

--- apps/frontend/web/src/config/global.ts
+++ apps/frontend/web/src/config/global.ts
@@ -3,9 +3,8 @@ export const siteConfig = {
 } as const;
 
 export const apiConfig = {
-  // TODO: Fix prod URL after FastAPI changes are finished
   baseUrl:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8080/api"
-      : "http://localhost:8080/api",
+      : "/api",
 } as const;
