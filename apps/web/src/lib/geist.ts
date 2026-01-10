74292e710095c375e884895b473aaa2bbc853def
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2026-01-10 14:16:51 +0545

    fix(web): Add display swap property to geist font
    https://github.com/vercel/next.js/discussions/48904

--- apps/web/src/lib/geist.ts
+++ apps/web/src/lib/geist.ts
@@ -1,11 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
+  display: "swap",
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
+  display: "swap",
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
